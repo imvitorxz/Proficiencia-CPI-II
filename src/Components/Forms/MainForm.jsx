5aa0ab44cff77e0cf745fdd02d11e033e9d68e7c
Vitor Colombelli <vittorcolom@gmail.com>
2025-05-08 16:23:26 -0300

    feat: atualizando css

--- src/Components/Forms/MainForm.jsx
+++ src/Components/Forms/MainForm.jsx
@@ -8,16 +8,16 @@ export default function MainForm() {
                 Seja bem-vindo ao nosso formulário! Aqui você pode se cadastrar e aproveitar todas as nossas funcionalidades. Preencha os campos abaixo para começar sua jornada conosco.
             </p>
             <div className={styles.formOneModule}>
-                <label className={styles.label} name="nome" id="nome">Nome:</label>
-                <input type="text"/>
+                <label className={styles.label} name="nome" id="nome">Nome</label>
+                <input className={styles.input} id="inputName" name="inputName" type="text" placeholder='Insira seu nome'/>
             </div>
             <div className={styles.formOneModule}>
-                <label className={styles.label} name="email" id="email">Email:</label>
-                <input type="email"/>
+                <label className={styles.label} name="email" id="email">Email</label>
+                <input className={styles.input} id="email" input="email" type="email" placeholder='Insira seu e-mail'/>
             </div>
             <div className={styles.formOneModule}>
-               <label className={styles.label} name="senha" id="senha">Senha:</label>
-                <input type="password"/>
+               <label className={styles.label} name="senha" id="senha">Senha</label>
+                <input className={styles.input} name="password" id="password" type="password" placeholder='Insira sua senha'/>
             </div>
         </form>
     )
