efd478a3acd00c3d107c5e2b0d67b7cf3c3b6dd1
Vitor Colombelli <vittorcolom@gmail.com>
2025-05-08 17:00:45 -0300

    feat: finalizando

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Header from './Components/Header/header';
 import Footer from './Components/Footer/footer';
 
-import ExibirCadastro from './Components/Buttons/ExibirCadastro';
 import MainForm from './Components/Forms/MainForm';
 
 function App() {
+  document.title = "Formulário de Cadastro - Proficência CPI II";
   return (
     <div className="App">
       <Header/>
       <MainForm/>
-      <ExibirCadastro/>
+      <showData/>
       <Footer/>
     </div>
   );
--- src/Components/Forms/MainForm.jsx
+++ src/Components/Forms/MainForm.jsx
@@ -1,6 +1,35 @@
 import styles from './MainForm.module.css'
 
+import { useState } from 'react';
+
+let formValidado = false;
+
 export default function MainForm() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function validateData(event) {
+        event.preventDefault();
+
+        if (name.length < 3) {
+            alert('Nome inválido!');
+            return;
+        }
+        if (email.length < 6) {
+            alert('Email inválido!');
+            return;
+        }
+        if (password.length < 6) {
+            alert('Senha inválida!');
+            return;
+        }
+
+        formValidado = true;
+        console.log(formValidado);
+    }
+
     return (
         <form className={styles.form}>
             <h2>Formulário</h2>
@@ -9,15 +38,37 @@ export default function MainForm() {
             </p>
             <div className={styles.formOneModule}>
                 <label className={styles.label} name="nome" id="nome">Nome</label>
-                <input className={styles.input} id="inputName" name="inputName" type="text" placeholder='Insira seu nome'/>
+                <input onChange={(e) => setName(e.target.value)} className={styles.input} id="inputName" name="inputName" type="text" placeholder='Insira seu nome' min={3} required/>
             </div>
             <div className={styles.formOneModule}>
                 <label className={styles.label} name="email" id="email">Email</label>
-                <input className={styles.input} id="email" input="email" type="email" placeholder='Insira seu e-mail'/>
+                <input onChange={(e) => setEmail(e.target.value)} className={styles.input} id="email" input="email" type="email" placeholder='Insira seu e-mail' min={6} required/>
             </div>
             <div className={styles.formOneModule}>
                <label className={styles.label} name="senha" id="senha">Senha</label>
-                <input className={styles.input} name="password" id="password" type="password" placeholder='Insira sua senha'/>
+                <input onChange={(e) => setPassword(e.target.value)} className={styles.input} name="password" id="password" type="password" placeholder='Insira sua senha' min={6} required/>
+            </div>
+            <button className={styles.button} onClick={validateData}>
+                Exibir cadastro
+            </button>
+
+            <div className={styles.divCadastroSucesso} style={{ display: formValidado ? 'none' : 'block' }}>
+                <h2>Cadastro realizado com sucesso!</h2>
+                <p>
+                    Seus dados foram validados e cadastrados com sucesso, verifique se estão corretos.
+                </p>
+                <h3>
+                    Nome
+                </h3>
+                <p>{name}</p>
+                <h3>
+                    Email
+                </h3>
+                <p>{email}</p>
+                <h3>
+                    Senha
+                </h3>
+                <p>não mostrada por questões de segurança.</p>
             </div>
         </form>
     )
